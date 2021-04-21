f78fd9dc0070ef2dae2a6f84177e3fef3793c302
ikarkaz <47292517+ikarkaz@users.noreply.github.com>
2021-04-21 09:57:13 +0300

    New homework

---
+++ test_main_page.py
@@ -0,0 +1,7 @@
+link = "http://selenium1py.pythonanywhere.com/"
+
+class MainPage():
+    def test_guest_can_go_to_login_page(browser):
+        browser.get(link)
+        login_link = browser.find_element_by_css_selector("#login_link")
+        login_link.click()
