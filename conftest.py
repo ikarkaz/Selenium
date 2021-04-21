6f018b7742533b4a99c30a6a8bf60ca30f3619fa
ikarkaz <ikarkaz@gmail.com>
2021-04-21 12:39:30 +0300

    Page_object_2

--- conftest.py
+++ conftest.py
@@ -12,6 +12,6 @@ def browser(request):
     options = Options()
     options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
     browser = webdriver.Chrome(options=options)
-    browser.implicitly_wait(35)
+    #browser.implicitly_wait(35)
     yield browser
     browser.quit()
