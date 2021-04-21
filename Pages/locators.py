6b3848a2b0faddc8b3beeb39a5db03e1b90cdd34
ikarkaz <ikarkaz@gmail.com>
2021-04-21 16:45:22 +0300

    Page_obj_first_swich_windows

--- Pages/locators.py
+++ Pages/locators.py
@@ -3,3 +3,9 @@ from selenium.webdriver.common.by import By
 
 class MainPageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+
+class LoginPageLocators():
+    LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
+    LOGIN_BTN = (By.NAME, "login_submit")
+    REGISTR_FORM = (By.CSS_SELECTOR,"#register_form")
+    REGISTR_BTN = (By.NAME, "registration_submit")
---
+++ Pages/login_page.py
@@ -0,0 +1,17 @@
+from .base_page import BasePage
+from .locators import LoginPageLocators
+
+class LoginPage(BasePage):
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self):
+        assert "login" in self.browser.current_url 
+
+    def should_be_login_form(self):
+        assert (self.browser.find_element(*LoginPageLocators.LOGIN_FORM)) and (self.browser.find_element(*LoginPageLocators.LOGIN_BTN)), "LOGIN not FOUND"
+
+    def should_be_register_form(self):
+        assert (self.browser.find_element(*LoginPageLocators.REGISTR_FORM)) and (self.browser.find_element(*LoginPageLocators.REGISTR_BTN)), "REGISTRATE not FOUND"
